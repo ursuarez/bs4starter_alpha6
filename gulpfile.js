6fb0341c4a907a8e481aad07bf36859e487c158d
Ricardo Suárez <ur.suarez@gmail.com>
2018-01-22 16:09:36 -0600

    custom gulpfile.js

--- gulpfile.js
+++ gulpfile.js
@@ -1,43 +1,58 @@
-const gulp        = require('gulp');
-const browserSync = require('browser-sync').create();
-const sass        = require('gulp-sass');
+const 
+    gulp         = require('gulp'),
+    browserSync  = require('browser-sync').create(),
+    sass         = require('gulp-sass'),
+    clean        = require('gulp-clean-css'),
+    csscomb      = require('gulp-csscomb'),
+    postcss      = require('gulp-postcss'),
+    autoprefixer = require('autoprefixer')
 
-// Compile Sass & Inject Into Browser
-gulp.task('sass', function() {
-    return gulp.src(['node_modules/bootstrap/scss/bootstrap.scss', 'src/scss/*.scss'])
+gulp.task('sass', () => {
+    var processors = [
+        autoprefixer({
+            browsers: ['>5%', 'ie 8']
+        })
+    ]
+
+    gulp.src(['node_modules/bootstrap/scss/bootstrap.scss', 'src/scss/*.scss'])
         .pipe(sass())
+        .pipe(postcss(processors))
+        .pipe(clean())
         .pipe(gulp.dest("src/css"))
-        .pipe(browserSync.stream());
-});
+        .pipe(browserSync.stream())
+})
 
-// Move JS Files to src/js
-gulp.task('js', function() {
-    return gulp.src(['node_modules/bootstrap/dist/js/bootstrap.min.js', 'node_modules/jquery/dist/jquery.min.js', 'node_modules/tether/dist/js/tether.min.js'])
+gulp.task('csscomb', () => {
+    gulp
+        .src('src/scss/*.scss')
+        .pipe(csscomb())
+        .pipe(gulp.dest('src/scss'))
+})
+
+gulp.task('js', () => {
+    gulp.src(['node_modules/bootstrap/dist/js/bootstrap.min.js', 'node_modules/jquery/dist/jquery.min.js', 'node_modules/tether/dist/js/tether.min.js'])
         .pipe(gulp.dest("src/js"))
-        .pipe(browserSync.stream());
-});
+        .pipe(browserSync.stream())
+})
 
-// Watch Sass & Serve
-gulp.task('serve', ['sass'], function() {
+gulp.task('serve', ['sass'], () => {
 
     browserSync.init({
         server: "./src"
-    });
+    })
 
-    gulp.watch(['node_modules/bootstrap/scss/bootstrap.scss', 'src/scss/*.scss'], ['sass']);
-    gulp.watch("src/*.html").on('change', browserSync.reload);
-});
+    gulp.watch(['node_modules/bootstrap/scss/bootstrap.scss', 'src/scss/*.scss'], ['sass', 'csscomb'])
+    gulp.watch('src/*.html').on('change', browserSync.reload)
+})
 
-// Move Fonts to src/fonts
-gulp.task('fonts', function() {
-  return gulp.src('node_modules/font-awesome/fonts/*')
+gulp.task('fonts', () => {
+  gulp.src('node_modules/font-awesome/fonts/*')
     .pipe(gulp.dest('src/fonts'))
 })
 
-// Move Font Awesome CSS to src/css
-gulp.task('fa', function() {
-  return gulp.src('node_modules/font-awesome/css/font-awesome.min.css')
+gulp.task('fa', () => {
+  gulp.src('node_modules/font-awesome/css/font-awesome.min.css')
     .pipe(gulp.dest('src/css'))
 })
 
-gulp.task('default', ['js','serve', 'fa', 'fonts']);
+gulp.task('default', ['js','serve', 'fa', 'fonts'])
